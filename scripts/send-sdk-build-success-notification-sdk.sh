5580ac2500b5fc133b300800e9dcaef711db06c0
Artur Rudiuk <a.rudiuk@criteo.com>
2021-11-09 17:56:27 +0200

    Update send-sdk-build-success-notification-sdk.sh

--- scripts/send-sdk-build-success-notification-sdk.sh
+++ scripts/send-sdk-build-success-notification-sdk.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
-SUITE_AND_RUN_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/workflows/$1/runs \
-                | jq -r '.workflow_runs[0] | ((.check_suite_id | tostring) + " "+ (.id | tostring))')"
-SUITE_AND_RUN_ID_ARRAY=($SUITE_AND_RUN_ID)
-SUITE_ID=${SUITE_AND_RUN_ID_ARRAY[0]}
-RUN_ID=${SUITE_AND_RUN_ID_ARRAY[1]}
-
-sleep 15
-
-echo "$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}/artifacts)"
-ARTIFACT_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}/artifacts \
-                | jq -r '.artifacts[0].id')"
+RUN_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/workflows/$1/runs \
+            | jq -r '.workflow_runs[0].id | tostring')"
 
 result=$(curl -X POST --data-urlencode \
 "payload={ \
     \"channel\": \"#criteo-api-sdk-generator\", \
     \"username\": \"sdk-generation-bot\", \
-    \"text\": \"$2 build succeeded.\n<https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build> \n \
-<https://github.com/criteo/criteo-api-sdk-generator/suites/${SUITE_ID}/artifacts/${ARTIFACT_ID}|Link to download the artifact>\", \
+    \"text\": \"$2 build succeeded.\n<https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build>\, \
     \"icon_emoji\": \":green_check_mark:\"}" \
     $3 )
