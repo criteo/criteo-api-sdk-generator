97e998f954bc897890310ccd93cb143a7f35ab30
Artur Rudiuk <a.rudiuk@criteo.com>
2021-11-09 17:24:45 +0200

    more fixes

--- scripts/send-sdk-build-success-notification-sdk.sh
+++ scripts/send-sdk-build-success-notification-sdk.sh
@@ -3,9 +3,9 @@ set -ex
 
 SUITE_AND_RUN_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/workflows/$1/runs \
                 | jq -r '.workflow_runs[0] | ((.check_suite_id | tostring) + " "+ (.id | tostring))')"
-
-SUITE_ID=($SUITE_AND_RUN_ID)[0]
-RUN_ID=($SUITE_AND_RUN_ID)[1]
+SUITE_AND_RUN_ID_ARRAY=($SUITE_AND_RUN_ID)
+SUITE_ID=${SUITE_AND_RUN_ID_ARRAY[0]}
+RUN_ID=${SUITE_AND_RUN_ID_ARRAY[1]}
 
 ARTIFACT_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}/artifacts \
                 | jq -r '.artifacts[0].id | tostring')"
@@ -14,6 +14,7 @@ $(curl -X POST --data-urlencode \
 "payload={ \
     \"channel\": \"#criteo-api-sdk-generator\", \
     \"username\": \"sdk-generation-bot\", \
-    \"text\": \"$2 build succeeded.\n Link to build https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID} \n \
-    Link to download the artifact https://github.com/criteo/criteo-api-sdk-generator/suites/${SUITE_ID}/artifacts/${ARTIFACT_ID}\", \
-    \"icon_emoji\": \":heavy_check_mark:\"}" https://hooks.slack.com/services/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 )
+    \"text\": \"$2 build succeeded.\n <https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build> \n \
+    <https://github.com/criteo/criteo-api-sdk-generator/suites/${SUITE_ID}/artifacts/${ARTIFACT_ID}|Link to download the artifact>\", \
+    \"icon_emoji\": \":heavy_check_mark:\"}" \
+    https://hooks.slack.com/services/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 )
