633276bf42f8ce6ab44e75f2f6aeab0e96c1753c
Artur Rudiuk <a.rudiuk@criteo.com>
2021-11-09 17:45:06 +0200

    More fixes

--- scripts/send-sdk-build-failure-notification.sh
+++ scripts/send-sdk-build-failure-notification.sh
@@ -4,10 +4,10 @@ set -ex
 RUN_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/workflows/$1/runs \
             | jq -r '.workflow_runs[0].id | tostring')"
 
-$(curl -X POST --data-urlencode \
+result=$(curl -X POST --data-urlencode \
 "payload={ \
     \"channel\": \"#criteo-api-sdk-generator\",\
     \"username\": \"sdk-generation-bot\", \
-    \"text\": \"$2 build failed.\n <https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build>, \
+    \"text\": \"$2 build failed.\n <https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build>\", \
     \"icon_emoji\": \":x:\"}" \
 $3)
--- scripts/send-sdk-build-success-notification-sdk.sh
+++ scripts/send-sdk-build-success-notification-sdk.sh
@@ -8,13 +8,13 @@ SUITE_ID=${SUITE_AND_RUN_ID_ARRAY[0]}
 RUN_ID=${SUITE_AND_RUN_ID_ARRAY[1]}
 
 ARTIFACT_ID="$(curl https://api.github.com/repos/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}/artifacts \
-                | jq -r '.artifacts[0].id | tostring')"
+                | jq -r '.artifacts[0].id')"
 
-$(curl -X POST --data-urlencode \
+result=$(curl -X POST --data-urlencode \
 "payload={ \
     \"channel\": \"#criteo-api-sdk-generator\", \
     \"username\": \"sdk-generation-bot\", \
-    \"text\": \"$2 build succeeded.\n <https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build> \n \
-    <https://github.com/criteo/criteo-api-sdk-generator/suites/${SUITE_ID}/artifacts/${ARTIFACT_ID}|Link to download the artifact>\", \
-    \"icon_emoji\": \":heavy_check_mark:\"}" \
+    \"text\": \"$2 build succeeded.\n<https://github.com/criteo/criteo-api-sdk-generator/actions/runs/${RUN_ID}|Link to build> \n \
+<https://github.com/criteo/criteo-api-sdk-generator/suites/${SUITE_ID}/artifacts/${ARTIFACT_ID}|Link to download the artifact>\", \
+    \"icon_emoji\": \":green_check_mark:\"}" \
     $3 )
