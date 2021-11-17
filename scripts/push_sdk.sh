ceb9fed4021bff9546e02b761e5cbde426647200
paul-mathon <p.mathon@criteo.com>
2021-11-17 13:28:53 +0100

    Git diff test

--- scripts/push_sdk.sh
+++ scripts/push_sdk.sh
@@ -76,7 +76,7 @@ copy_new_sdks() {
     echo "[INFO] Directory $sdks_directory created."
   fi
 
-  cp -r "$GENERATOR_REPO_DIR/generated-sources/$LANGUAGE" "$SDK_REPO_DIR/sdks"
+  cp -r "$GENERATOR_REPO_DIR/generated-sources/$LANGUAGE/" "$SDK_REPO_DIR/sdks"
 
   echo "[INFO] Copy successful."
 }
@@ -103,6 +103,8 @@ git_commit_and_tag() {
 
 git_push() {
   git push origin --quiet
+  # Push tag
+  git push origin --tags --quiet
 }
 
 process() {
