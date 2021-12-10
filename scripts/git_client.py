26f4bed804e6d5803212105246779b1a295bcba5
Paul Mathon <paul.mathon@outlook.fr>
2021-12-10 11:18:46 +0100

    Cat pk'

--- scripts/git_client.py
+++ scripts/git_client.py
@@ -45,6 +45,7 @@ class GitClient2:
         with open(pk_path, 'w') as f:
             f.write(self.private_key)
         
+        utils.run_command(f'cat {pk_path}')
         utils.run_command(f'chmod 600 {pk_path}')
         
         Repo.clone_from(f'git@github.com:{organization}/{repository}.git', repository, env= { 'GIT_SSH_COMMAND': f'ssh -i {pk_path}'})
