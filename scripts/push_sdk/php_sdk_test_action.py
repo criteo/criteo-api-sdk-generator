bcaa8e373765f7a0115040176ea76e36afa029b2
paul-mathon <p.mathon@criteo.com>
2021-12-28 11:31:16 +0100

    Run tests as python process instead of subprocess

--- scripts/push_sdk/php_sdk_test_action.py
+++ scripts/push_sdk/php_sdk_test_action.py
@@ -10,11 +10,11 @@ class PhpSdkTestAction:
     self.logger.info(f'Starting testing SDK {sdk_name}...')
 
     self.logger.info('Installing dependencies...')
-    utils.run_command('composer install')
+    os.system('composer install')
     self.logger.info('Install successful')
 
     self.logger.info('Testing...')
-    utils.run_command('composer test', env={
+    os.system('composer test', env={
         'TEST_CLIENT_ID': utils.assert_environment_variable('TEST_CLIENT_ID'),
         'TEST_CLIENT_SECRET': utils.assert_environment_variable('TEST_CLIENT_SECRET'),
         'TEST_APPLICATION_ID': utils.assert_environment_variable('TEST_APPLICATION_ID'),
