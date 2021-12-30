c0aba276ede652692217b2c6d2b321d2c507061c
paul-mathon <p.mathon@criteo.com>
2021-12-30 15:19:40 +0100

    Stop SDKs generation on test failure

--- scripts/push_sdk/php_sdk_test_action.py
+++ scripts/push_sdk/php_sdk_test_action.py
@@ -1,14 +1,11 @@
 import os
-from os import path
 
-from push_sdk.clients.os_client import IOsClient
 from push_sdk.clients.fs_client import IFsClient
 from push_sdk import utils
 
 class PhpSdkTestAction:
-  def __init__(self, os_client: IOsClient, fs_client: IFsClient):
+  def __init__(self, fs_client: IFsClient):
     self.logger = utils.get_logger()
-    self.os_client = os_client
     self.fs_client = fs_client
 
   def execute(self, sdk_name):
@@ -22,11 +19,7 @@ class PhpSdkTestAction:
     exit_code += os.system('composer test')
 
     if exit_code > 0:
-      self.logger.error(f'Test Action failed for SDK {sdk_name}, removing it...')
-      sdk_path = path.join(self.os_client.get_generated_sources_base_path('php'), sdk_name)
-      self.fs_client.remove(sdk_path)
-      self.logger.info('Removal successful')
-      return
+      raise Exception(f'Test Action failed for SDK {sdk_name}')
 
     self.logger.info('Test successful')
 
