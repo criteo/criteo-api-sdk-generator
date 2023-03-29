aae19b3c031c4093cafcb625b837e7edc4ec3358
Ishan Gautam <i.gautam@criteo.com>
2023-03-29 11:45:43 +0200

    Update java sandbox

--- sandbox/java/src/main/java/com/criteo/app/App.java
+++ sandbox/java/src/main/java/com/criteo/app/App.java
@@ -1,6 +1,7 @@
 package com.criteo.app;
 
 import com.criteo.api.marketingsolutions.v2022_07.ApiClient;
+import com.criteo.api.marketingsolutions.v2022_07.ApiClientBuilder;
 import com.criteo.api.marketingsolutions.v2022_07.ApiException;
 import com.criteo.api.marketingsolutions.v2022_07.Configuration;
 import com.criteo.api.marketingsolutions.v2022_07.auth.*;
@@ -9,16 +10,17 @@ import com.criteo.api.marketingsolutions.v2022_07.api.AdvertiserApi;
 
 public class App {
     public static void main(String[] args) {
-        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        
+        ApiClient defaultClient = ApiClientBuilder.BuildDefaultClient();
         
         // Configure OAuth2, two options:
         // 1. Set your access token manually, refresh token mechanism IS NOT handled by the client
-        OAuth oauth = (OAuth) defaultClient.getAuthentication("oauth");
-        oauth.setAccessToken("YOUR_TOKEN");
+        // OAuth oauth = (OAuth) defaultClient.getAuthentication("oauth");
+        // oauth.setAccessToken("YOUR_TOKEN");
 
         // 2. Set your credentials within the ApiClient, refresh token mechanism IS handled for you 💚
-        // defaultClient.setUsername("YOUR_CIENT_ID");
-        // defaultClient.setPassword("YOUR_CLIENT_SECRET");
+        defaultClient.setUsername("YOUR_CIENT_ID");
+        defaultClient.setPassword("YOUR_CLIENT_SECRET");
 
         AdvertiserApi apiInstance = new AdvertiserApi(defaultClient);
         try {
